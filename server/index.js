c9501818c16b633c054e2b5c4555dbb3f1d2b4ae
randomsapiens1 <imsapiens.ctf@gmail.com>
2024-06-04 08:13:23 +0600

    apollo_server

--- server/index.js
+++ server/index.js
@@ -7,10 +7,42 @@ import bodyParser from 'body-parser';
 
 async function startServer() {
     const app = express();
-    const server = new ApolloServer({});
-    
+    const server = new ApolloServer({
+        typeDefs: `
+         type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean
+         }
+
+         type Query {
+            getTodos: [Todo]
+         }
+        `,
+        resolvers: {
+            Query: {
+                getTodos: () => [
+                    { id: '1', title: 'something something', completed: false },
+                ],
+            },
+        },
+    });
+
+    //middleware
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start()
+
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(3000, ()=>
+        console.log('Server running on port 3000')
+    );
 
 }
+
+startServer();
 
 
 
