327dd1b9907cd65de0f6cb103d2739f48ba71d08
randomsapiens1 <imsapiens.ctf@gmail.com>
2024-06-06 02:09:58 +0600

    need-more-change

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-//https://www.youtube.com/watch?v=WtkKwO1viI8&t=165s
-
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { default: axios} from 'axios';
-
+import axios from 'axios';
 
 async function startServer() {
     const app = express();
@@ -24,26 +21,35 @@ async function startServer() {
         `,
         resolvers: {
             Query: {
-                getTodos: async ()=> await axios.get('https://jsonplaceholder.typicode.com/todos/')
+                getTodos: async () => {
+                    try {
+                        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+                        console.log('Fetched data:', response.data); // Log the fetched data
+                        return response.data.map(todo => ({
+                            id: todo.id.toString(), // Ensure ID is a string
+                            title: todo.title,
+                            completed: todo.completed
+                        }));
+                    } catch (error) {
+                        console.error('Error fetching todos:', error);
+                        throw new Error('Unable to fetch todos');
+                    }
+                },
             },
-        },
+        }
     });
 
-    //middleware
+    // Middleware
     app.use(bodyParser.json());
     app.use(cors());
 
-    await server.start()
+    await server.start();
 
     app.use("/graphql", expressMiddleware(server));
 
-    app.listen(3000, ()=>
+    app.listen(3000, () => 
         console.log('Server running on port 3000')
     );
-
 }
 
 startServer();
-
-
-
